be96d7ddccdc65d7aee841440273d06f549a57f0
AlanCapilla <118011904+AlanCapilla@users.noreply.github.com>
2024-06-05 01:52:06 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        //VentanaLog ventanaLog =  new VentanaLog();
-        //ventanaLog.setVisible(true);
+        VentanaLog ventanaLog =  new VentanaLog();
+        ventanaLog.setVisible(true);
 
-        VentanaChat ventanaChat =  new VentanaChat();
-        ventanaChat.setVisible(true);
     }
 }
