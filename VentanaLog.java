133e8d4ed27355ed89258398ae3ce994995b78ff
AlanCapilla <118011904+AlanCapilla@users.noreply.github.com>
2024-06-06 01:44:50 -0600

    Update VentanaLog.java

--- VentanaLog.java
+++ VentanaLog.java
@@ -14,7 +14,6 @@ import java.awt.event.ActionListener;
  */
 public class VentanaLog extends JFrame {
     private VentanaChat ventanaChat =  new VentanaChat();
-
     private JButton chatear;
     private TextField apodo;
     private JLabel text_us, text_chat;
@@ -27,35 +26,7 @@ public class VentanaLog extends JFrame {
         setTitle("Login");
         this.getContentPane().setBackground(Color.decode("#FFFFFF"));
 
-        componentsGIU();
-    }
-
-    public void componentsGIU(){
-        botones();
-        labels();
-        textfiel();
-    }
-
-    public void botones(){
-        chatear = new JButton("Chatear");
-        chatear.setBounds(230,170,100,30);
-        add(chatear);
-
-        chatear.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-                ventanaChat.setVisible(true);
-            }
-        });
-    }
-    public void textfiel(){
-        apodo = new TextField();
-        apodo.setBounds(210,90,150,25);
-        add(apodo);
-
-    }
-    public void labels(){
+        
         /**
          * Creamos el texto donde pondremos el nombre del chat,
          * ademas de poner el texto de complemento para el campo vacio
@@ -68,15 +39,60 @@ public class VentanaLog extends JFrame {
         text_chat.setFont(new Font("comic sans ms",Font.ITALIC,30));
         add(text_chat);
 
+
         //Nickname
         text_us = new JLabel("Apodo: ");
         text_us.setBounds(120,90,80,25);
         text_us.setFont(new Font("Kristen ITC",Font.ITALIC,20));
         add(text_us);
 
+        /**
+         * Agregamos los textField donde almacenaremos el nombre del usuario
+         */
+        apodo = new TextField();
+        apodo.setBounds(210,90,150,25);
+        add(apodo);
+
+        /**
+         * Creamos el boton y el evento que llamara un metodo
+         * para acceder al chat
+         */
+        chatear = new JButton("Chatear");
+        chatear.setBounds(230,170,100,30);
+        add(chatear);
+
+
+        /**
+         * Apartado para los evetos
+         */
+
+        chatear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                entrarChat();
+            }
+        });
+
+
+    }
+
+
+    /**
+     * Metodo para acceder al chat,
+     * en esta parte se verifica que el text fiel no esta vacio, en dado caso esta no entra,
+     * si tiene texto entra
+     */
+    public void entrarChat(){
+       String tx_Apodo = apodo.getText().trim();
+
+       if (!tx_Apodo.isEmpty()){
+           dispose();
+           ventanaChat.setVisible(true);
+       }
     }
 
 
 
 }
+
 
